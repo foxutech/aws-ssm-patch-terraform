fcfc009dcb02a0d5fec27808be4c4d8004cf2a11
FoxuTech <prabulugar@gmail.com>
2018-09-03 23:55:19 +0530

    Delete awsenv.sh

--- awsenv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-read -rep "Enter your AWS accesskey: /n" accessKey
-
-read -rep "Enter your AWS secretkey: /n" secretKey
-
-export AWS_ACCESS_KEY_ID=$accessKey
-export AWS_SECRET_ACCESS_KEY=$secretKey
